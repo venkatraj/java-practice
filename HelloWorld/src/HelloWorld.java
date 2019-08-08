35f1da4300951c461b674f4ef5784bb5b36697aa
Venkat <venkat@codinggeek.com>
2019-08-08 22:31:11 +0530

    first variable

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFirstNumber = 17;
+        System.out.println(myFirstNumber);
     }
 }
