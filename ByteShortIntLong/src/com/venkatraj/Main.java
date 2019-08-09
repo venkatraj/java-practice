6eee8c9c39eb034ea48b85296c9d4a9851228dd4
Venkat <venkat@codinggeek.com>
2019-08-09 11:43:45 +0530

    byte, short and long min max values

--- ByteShortIntLong/src/com/venkatraj/Main.java
+++ ByteShortIntLong/src/com/venkatraj/Main.java
@@ -3,10 +3,8 @@ package com.venkatraj;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         int myIntMinValue = Integer.MIN_VALUE;
         int myIntMaxValue = Integer.MAX_VALUE;
-
         System.out.println("Integer Minimum Value = " + myIntMinValue);
         System.out.println("Integer Maximum Value = " + myIntMaxValue);
 
@@ -18,5 +16,26 @@ public class Main {
 
         int myIntEasyReadable = 2_147_483_647;
         System.out.println("Easy readable, but standard in computation = " + myIntEasyReadable);
+
+        byte myByteMinValue = Byte.MIN_VALUE;
+        byte myByteMaxValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myByteMinValue);
+        System.out.println("Byte Maximum Value = " + myByteMaxValue);
+
+
+        short myShortMinValue = Short.MIN_VALUE;
+        short myShortMaxValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myShortMinValue);
+        System.out.println("Short Maximum Value = " + myShortMaxValue);
+
+        long myLongMinValue = Long.MIN_VALUE;
+        long myLongMaxValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myLongMinValue);
+        System.out.println("Long Maximum Value = " + myLongMaxValue);
+
+        // Long should be prefixed with L otherwise converted to Integer
+        long myLongWithSmallValue = 100L;
+
+
     }
 }
