360af54ca3f05730d176f0c4bb91478acb4e68d4
Venkat <venkat@codinggeek.com>
2019-08-09 15:32:48 +0530

    Char and Boolean

---
+++ CharAndBoolean/src/com/venkatraj/Main.java
@@ -0,0 +1,17 @@
+package com.venkatraj;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = 'D';
+	    char myUnicodeChar = '\u0044';
+        System.out.println("myChar = " + myChar);
+        System.out.println("myUnicodeChar = " + myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println("myCopyrightChar = " + myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isOverEighteen = true;
+    }
+}
