6ad330e99d3d1d1f20c6c321b8fb8eb4d064a953
Venkat <venkat@codinggeek.com>
2019-08-10 14:40:47 +0530

    Teen number checker

---
+++ Section4Exercises/src/com/venkatraj/TeenNumberChecker.java
@@ -0,0 +1,17 @@
+package com.venkatraj;
+
+public class TeenNumberChecker {
+    public static boolean hasTeen(int age1, int age2, int age3) {
+        if (isTeen(age1) || isTeen(age2) || isTeen(age3)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isTeen(int age) {
+        if (age >= 13 && age <= 19) {
+            return true;
+        }
+        return false;
+    }
+}
