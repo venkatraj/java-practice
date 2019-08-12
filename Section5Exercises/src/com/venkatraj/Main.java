b9cb3569de2f541564fab1ad78e3733e7e762ee8
Venkat <venkat@codinggeek.com>
2019-08-12 16:35:25 +0530

    switch practice

---
+++ Section5Exercises/src/com/venkatraj/Main.java
@@ -0,0 +1,8 @@
+package com.venkatraj;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Switch.findChar();
+    }
+}
---
+++ Section5Exercises/src/com/venkatraj/Switch.java
@@ -0,0 +1,29 @@
+package com.venkatraj;
+
+public class Switch {
+    public static void findChar() {
+        int myChar = 'C';
+        switch(myChar) {
+            case 'A':
+                System.out.println("It was A");
+                break;
+            case 'B':
+                System.out.println("It was B");
+                break;
+            case 'C':
+                System.out.println("It was C");
+                break;
+            case 'D':
+                System.out.println("It was D");
+                break;
+            case 'E':
+                System.out.println("It was E");
+                break;
+            default:
+                System.out.println("It was not A,B,C,D or E");
+                break;
+
+        }
+
+    }
+}
