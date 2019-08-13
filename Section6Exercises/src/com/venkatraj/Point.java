b5a47cdfcda323c09ca92177fb0890b88c5c6b32
Venkat <venkat@codinggeek.com>
2019-08-13 16:39:06 +0530

    Point exercise

---
+++ Section6Exercises/src/com/venkatraj/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point p) {
+        return this.distance(p.x, p.y);
+    }
+
+    public double distance() {
+        return this.distance(0, 0);
+    }
+}
