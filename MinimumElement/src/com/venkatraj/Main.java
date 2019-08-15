5363da79f214b182b34e8a5d779a00dae65866aa
Venkat <venkat@codinggeek.com>
2019-08-15 13:24:04 +0530

    array reverse

--- MinimumElement/src/com/venkatraj/Main.java
+++ MinimumElement/src/com/venkatraj/Main.java
@@ -1,5 +1,6 @@
 package com.venkatraj;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,14 +11,18 @@ public class Main {
         System.out.println("Enter no. of elements\r");
         int number = scanner.nextInt();
         System.out.println("Enter the elements...\r");
-	    int[] intArray = readIntegers(number);
-	    int min = findMin(intArray);
+        int[] intArray = readIntegers(number);
+        int min = findMin(intArray);
         System.out.println("Minimum element in the array is " + min);
+        int[] someInts = new int[]{1, 2, 3, 4, 5};
+        System.out.println("Original Array: " + Arrays.toString(someInts));
+        reverse(someInts);
+        System.out.println("Reversed Array: " + Arrays.toString(someInts));
     }
 
     public static int[] readIntegers(int number) {
         int[] values = new int[number];
-        for(int i=0; i<number; i++) {
+        for (int i = 0; i < number; i++) {
             values[i] = scanner.nextInt();
         }
         return values;
@@ -25,11 +30,21 @@ public class Main {
 
     public static int findMin(int[] arr) {
         int min = arr[0];
-        for(int i=1; i< arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (min > arr[i]) {
                 min = arr[i];
             }
         }
         return min;
+    }
+
+    public static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int midPoint = array.length / 2;
+        for (int i = 0; i < midPoint; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
     }
 }
